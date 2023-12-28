7af52d8f21c74f98e97ce32eba1dd3f2a86134b8
8467042733 <rupeshpal25112004@gmail.com>
2023-12-28 19:08:55 +0530

    add some code

--- src/app.js
+++ src/app.js
@@ -12,6 +12,13 @@ const server = http.createServer((req, res) => {
       const value = obj.num1;
     
      // Write the code here to check if the number is odd or even
+     if(value%2==0){
+      res.writeHead(200, { "Content-Type":"text/plain" });
+      res.end(`The number ${value} is even`);
+    }else{
+      res.writeHead(404, { "Content-Type":"text/plain" });
+      res.end(`The number ${value} is odd`);
+    }
 
    });
   }
